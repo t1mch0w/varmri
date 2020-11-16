1d5da710d8e8dd620150b418d32ff318c419b8b0
fzhou <fzhou@node0.fzhou-qv84339.osu-nfs-test-pg0.wisc.cloudlab.us>
2020-11-16 09:08:44 -0700

    First commit for scheduler

---
+++ bin/run_test.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+test_id=$1
+test_type=$2
+num_nodes=$3
+
+#Apply cluster
+echo "[test${test_id}] apply the cluster"
+#./startExperiment --project osu-nfs-test --name test${test_id} --duration 16 --bindings="{\"NumberOfNodes\":\"$1\"}" osu-nfs-test,c220g1
+./startExperiment --project osu-nfs-test --name test${test_id} --duration 16 --bindings="{\"NumberOfNodes\":\"${num_nodes}\"}" osu-nfs-test,c220g5
+if ! [ $? -eq 0 ]
+then
+return 1
+fi
+
+#Check status
+echo "[test${test_id}] start to check status"
+while true
+do
+sleep 30
+#Get node status
+status=$(./experimentStatus osu-nfs-test,test${test_id} | grep Status | awk '{print $2}')
+#Add failed processing
+if [ ${status} = "failed" ]
+then
+./terminateExperiment osu-nfs-test,test${test_id}
+return 1
+elif [ ${status} = "ready" ]
+then
+break
+fi
+done
+
+#Init each node
+./experimentManifests osu-nfs-test,test${test_id} > test${test_id}.xml
+nodes=$(python get_allhosts.py test${test_id}.xml)
+for node in ${nodes}
+do
+echo "[test${test_id}] start to init"
+ssh ${node} -o StrictHostKeyChecking=no "cd /proj/osu-nfs-test-PG0/cloudlab_var_script;source init.sh" &
+done
+wait
+
+#Get node0
+./experimentManifests osu-nfs-test,test${test_id} > test${test_id}.xml
+node0=$(python get_hostname.py test${test_id}.xml)
+
+#Run the test
+echo "[test${test_id}] start to test"
+ssh ${node0} -o StrictHostKeyChecking=no "cd /proj/osu-nfs-test-PG0/cloudlab_var_script;./run_test.sh ${test_type} > test_${test_id}_type_${test_type}.log 2>&1"
+
+#Store results to NFS
+target_dir=/nfs/test${test_id}
+mkdir ${target_dir} 
+mv /mnt/asplos2021/varmri/userapp/*-*-*-* ${target_dir}/
+mv test_${test_id}_type_${test_type}.log ${target_dir}/
+mv test${test_id}.xml ${target_dir}/
+echo ${nodes} > ${target_dir}/cluster_info
+
+#Terminate the clusters
+./terminateExperiment osu-nfs-test,test${test_id}
+#Wait for 5 minutes for node recovery
+sleep 300
---
+++ bin/scheduler.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+export PYTHONPATH="/mnt/varcloud/emulab-xmlrpc"
+
+avai_nodes=5
+test_id=0
+declare -A cluster_info
+
+get_num_nodes () {
+test_type=$1
+if [ "${test_type}" = "0" ] || [ "${test_type}" = "1" ] || [ "${test_type}" = "3" ] || [ "${test_type}" = "4" ]
+then
+num_nodes=4
+elif [ "${test_type}" = "5" ]
+then
+num_nodes=5
+elif [ "${test_type}" = "2" ]
+then
+num_nodes=2
+else
+num_nodes=1
+fi
+echo ${num_nodes}
+}
+
+run_test () {
+test_id=$1
+test_type=$2
+num_nodes=$3
+avai_nodes=$(echo "${avai_nodes}-${num_nodes}" | bc)
+source ./run_test.sh ${test_id} ${test_type} ${num_nodes} &
+echo "[scheduler] test${test_id} starts. threadId= $! test_type= ${test_type} num_nodes= ${num_nodes} avai_nodes= ${avai_nodes}"
+cluster_info["$!"]=${num_nodes}
+}
+
+update_test () {
+for tid in "${!cluster_info[@]}"
+do
+if ! [ -d /proc/${tid} ]
+then
+avai_nodes=$(echo "${avai_nodes}+${cluster_info[${tid}]}" | bc)
+echo "[scheduler] one test finishes. threadId= $tid num_nodes= ${cluster_info[${tid}]} avai_nodes= ${avai_nodes}"
+unset cluster_info[${tid}]
+fi
+done
+}
+
+while true
+do
+#get test type and required number of nodes
+test_type=$(echo $RANDOM % 8 | bc)
+num_nodes=$(get_num_nodes ${test_type})
+
+#wait until we have enough nodes
+while [ "${num_nodes}" -gt "${avai_nodes}" ]
+do
+echo "[scheduler] test${test_id} test_type= ${test_type} num_nodes= ${num_nodes} avai_nodes= ${avai_nodes}"
+sleep 30
+update_test
+done
+run_test ${test_id} ${test_type} ${num_nodes}
+test_id=$(echo "${test_id}+1" | bc)
+
+#sleep for 5 sec, in case two requests of applying clusters are too close.
+sleep 5
+done
